8862866c112278cf5d7dce3a43715d1b9c3a0376
gloryoftan <916627606@qq.com>
2019-01-28 15:18:28 +0800

    NodeOperation

---
+++ Js/2019-01-28/节点操作（基本）/js/Node-Operation.js
@@ -0,0 +1,36 @@
+let aSpan = [...document.querySelectorAll("#wrap .content .left .items .city span")];
+let oSelected = document.querySelector("#wrap .content .left .items .selected");
+let oRightItems = document.querySelector("#wrap .right .items");
+
+let ifCreated = [];//判断是否已经选中
+
+//添加点击事件
+aSpan.forEach((ele, index) => {
+    ele.onclick = function () {
+        if (ifCreated[index]) return;
+        //生成左侧标签<span>London丨<i>X</i></span>
+        let oSpanLeft = document.createElement("span");
+        let txt = document.createTextNode(this.innerHTML + "丨");
+        let oI = document.createElement("i");
+        oI.innerHTML = "X";
+        oSpanLeft.appendChild(txt);
+        oSpanLeft.appendChild(oI);
+        
+        //生成右侧标签<span>Rome</span>
+        let oSpanRight = document.createElement("span");
+        oSpanRight.innerHTML = this.innerHTML;
+        
+         //给x加点击事件
+         oI.onclick =function(){
+            oSelected.removeChild(oSpanLeft);
+            oRightItems.removeChild(oSpanRight);
+            ifCreated[index] = false;
+        };
+
+        //渲染
+        oSelected.appendChild(oSpanLeft);
+        oRightItems.appendChild(oSpanRight);
+
+        ifCreated[index] = true;
+    };
+});
