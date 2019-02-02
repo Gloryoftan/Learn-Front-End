c0c482caa24ac10cb182db8b4845541a8985db48
gloryoftan <916627606@qq.com>
2019-02-02 22:30:29 +0800

    jQtest

--- jQuery/Test/js/test.js
+++ jQuery/Test/js/test.js
@@ -43,3 +43,144 @@ JS -> jQ $(JS对象)
 //     console.log(this);//this指向js对象（箭头函数没有this）
 //     $(this).css("color", "red");
 // });
+
+// //jQ里的Ajax
+// $.ajax({
+//     type: "GET",
+//     url: "DATA.txt",
+//     // crossOrigin: null,
+//     data: {},
+//     dataType: "JSON",
+//     success: function (msg) {
+//         console.log(msg);
+//     },
+//     error: function () {
+
+//     }
+// });
+// //Ajax的简写方式
+// $.get("DATA.txt", function (msg) {
+//     console.log(msg);
+// });
+
+// $.post("DATA.txt", {}, function (msg) {
+
+// });
+
+// //设置属性 返回jQ对象
+// $("#wrap").attr("attr", "value");
+
+// $("#wrap").attr({
+//     "attr1": "value1",
+//     "attr2": "value2"
+// });
+
+// //获取属性
+// $("#wrap").attr("id");
+
+// //移除属性
+// $("wrap").removeAttr("id");
+
+// //prop操作合法属性,操作js里表示真假值的时候可以使用
+// $("input").prop("checked");
+
+
+
+// /* classList
+// add
+// remove
+// toggle
+// contains
+// */
+// let $wrap = $("#wrap");
+
+// $wrap.addClass("attr1 attr2");
+
+// $wrap.removeClass("attr2");
+// $wrap.toggleClass("attr3 attr4");
+
+// $wrap.hasClass("attr1"); //返回bool
+
+// /* 
+// .html
+// ,text
+// .value
+// */
+
+// $("#wrap").html(); //获取
+// $("#wrap").html("ABC");
+
+// // jQ里的 +=
+// $("#wrap").html($("#wrap").html() + 1);
+
+// /* css */
+// $("#wrap").css({
+//     color: "red",
+//     fontWeight: "bolder",
+//     fontSize: "16px"
+// });
+
+// //获取css
+// $("#wrap").offset(); //自身到浏览器的距离，自身
+// $("#wrap").position(); //元素距离父级的距离，从margin的边缘开始算。
+
+// $("#wrap").offset({
+//     top: 50,
+//     left: 50
+// });
+
+// $("#wrap").offset().left;
+
+// //scroll属性
+// $("html").scrollTop() || $("body").scrollTop();
+
+// $(document).click(function () {
+//     $("html").scrollTop(450);
+// }); //放在点击事件里的原因是因为防止页面加载未完成就先执行
+
+// //获取值的区别
+// $("#wrap").width(); //值
+// $("#wrap").css("width"); //带px的字符串
+
+// $("#wrap").innerWidth();
+// $("#wrap").outerWidth(includeMargin);
+
+// /* 文档处理 */
+// /* 
+// appendChild
+// */
+
+// $("wrap").append("ABC");
+// $("wrap").append("<b>ABC</b>");
+
+// let a = document.createElement("b");
+// $("wrap").append(a);
+
+// $("wrap").append($("wrap1"));
+
+// //创建对象
+// let $b = $("<b class='className' id ='id'>ABC</b>");
+
+// //一些节点操作
+// let $dom = $(
+//     `<b>ABC</b>
+//     <div id="box">ABC</div>
+//     <a></a>`
+// );
+// $dom.eq(0).css("color", "red");
+// $dom.eq(2).html("This is a Div label");
+// $("wrap").append($dom);
+
+//appendTo 添加了兄弟
+//appenAfter 添加了子元素
+
+//empth = innerHtml =  "";
+
+//remove = 自杀;
+
+//filter();
+//has();
+
+
+
+
