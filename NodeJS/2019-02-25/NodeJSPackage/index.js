0a6fe0cd56cfc74cbaa58c3afede68f7f1186377
gloryoftan <916627606@qq.com>
2019-02-26 16:07:39 +0800

    EJS Public static

--- NodeJS/2019-02-25/NodeJSPackage/index.js
+++ NodeJS/2019-02-25/NodeJSPackage/index.js
@@ -3,6 +3,9 @@ const express = require("express");
 let app = express();
 app.listen(8888);
 
+//公开资源，用于读取css、js，存入静态资源库
+app.use(express.static("./public"));
+
 //设置当前服务运用的模版引擎
 app.set("view engine", "ejs");
 
