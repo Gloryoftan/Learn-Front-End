fa1fd7bbe1c89e9bc90deacd4e605ffa9fe0a788
gloryoftan <916627606@qq.com>
2019-02-26 10:17:47 +0800

    Install Cloud Service

---
+++ NodeJS/2019-02-26/Install Cloud Service.js
@@ -0,0 +1,24 @@
+/* 
+    *重置实例密码
+    *xshell 连接实例
+    
+    *安装npm
+        #yum install npm
+    *检测npm是否安装成功
+        #npm -v
+    *使用npm安装nodejs的版本控制器 n
+        #npm i n -g
+    *检测n是否安装成功
+        #n -V
+    *使用n安装node
+        #n stable
+        #n latest
+        #n 10.1.0
+    *n 回车切换版本
+    *使用n命令删除某个版本的弄的
+        #n rm10.1.0
+    *检测node是否安装成功
+        #node -v
+
+
+*/
