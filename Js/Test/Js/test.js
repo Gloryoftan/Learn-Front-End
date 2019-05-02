054abd0e377dbc30b6d95c40e634083a35511535
gloryoftan <916627606@qq.com>
2019-05-02 09:29:21 +0800

    test

--- Js/Test/Js/test.js
+++ Js/Test/Js/test.js
@@ -1198,26 +1198,26 @@ Tips：这些函数首字母需要大写（作为区分）
 // {   
 //     var str = formatStr;   
 //     var Week = ['日','一','二','三','四','五','六'];  
-  
+
 //     str=str.replace(/yyyy|YYYY/,this.getFullYear());   
 //     str=str.replace(/yy|YY/,(this.getYear() % 100)>9?(this.getYear() % 100).toString():'0' + (this.getYear() % 100));   
-  
+
 //     str=str.replace(/MM/,this.getMonth()>9?this.getMonth().toString():'0' + this.getMonth());   
 //     str=str.replace(/M/g,this.getMonth());   
-  
+
 //     str=str.replace(/w|W/g,Week[this.getDay()]);   
-  
+
 //     str=str.replace(/dd|DD/,this.getDate()>9?this.getDate().toString():'0' + this.getDate());   
 //     str=str.replace(/d|D/g,this.getDate());   
-  
+
 //     str=str.replace(/hh|HH/,this.getHours()>9?this.getHours().toString():'0' + this.getHours());   
 //     str=str.replace(/h|H/g,this.getHours());   
 //     str=str.replace(/mm/,this.getMinutes()>9?this.getMinutes().toString():'0' + this.getMinutes());   
 //     str=str.replace(/m/g,this.getMinutes());   
-  
+
 //     str=str.replace(/ss|SS/,this.getSeconds()>9?this.getSeconds().toString():'0' + this.getSeconds());   
 //     str=str.replace(/s|S/g,this.getSeconds());   
-  
+
 //     return str;   
 // };
 
@@ -1271,3 +1271,17 @@ Tips：这些函数首字母需要大写（作为区分）
 //  var klt=weight/(stature*stature);
 //  klt=klt>25?"胖":klt<20?"瘦":"正常";
 //  document.write(klt);
+
+
+
+window.onscroll = function () {
+    //变量t是滚动条滚动时，距离顶部的距离
+    var t = document.documentElement.scrollTop || document.body.scrollTop;
+    var scrollup = document.getElementById('scrollup');
+    //当滚动到距离顶部200px时，返回顶部的锚点显示
+    if (t >= 200) {
+        scrollup.style.display = "block";
+    } else { //恢复正常
+        scrollup.style.display = "none";
+    }
+}
