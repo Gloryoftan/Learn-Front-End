e425eff7dbbee05f8a535edcef244a29007a3f64
gloryoftan <916627606@qq.com>
2019-05-16 09:01:30 +0800

    canvas

--- layUI/js/layUItest.js
+++ layUI/js/layUItest.js
@@ -1,6 +1,19 @@
-layui.use(['layer', 'form'], function () {
-    var layer = layui.layer,
-        form = layui.form;
+//全局配置
+layui.config({
+    base: '../lib/js/modules/', //你存放新模块的目录，注意，不是layui的模块目录
+    version: false, //一般用于更新模块缓存，默认不开启。设为true即让浏览器不缓存。也可以设为一个固定的值，如：201610
+    debug: false, //用于开启调试模式，默认false，如果设为true，则JS模块的节点会保留在页面
+});
 
-    layer.msg('Hello World');
+//加载模块
+layui.use(['layer', 'form', 'element','index'], function () {
+    var form = layui.form; //获取form模块
+    var element = layui.element; //获取element模块
+    var layer = layui.element; //获取layer模块
+    var index = layui.index; //获取index模块
+
+    console.log(index);
+    console.log(form);
+    
+ 
 });
