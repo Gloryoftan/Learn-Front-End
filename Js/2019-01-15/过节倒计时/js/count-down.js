e673204ab87c1036ecc85f4be3d9dbb54ac27775
Gloryoftan <916627606@qq.com>
2019-07-23 14:53:39 +0800

    some tests

--- Js/2019-01-15/过节倒计时/js/count-down.js
+++ Js/2019-01-15/过节倒计时/js/count-down.js
@@ -1,20 +1,20 @@
 let oWrap = document.getElementById("wrap");
-let x = new Date(2019, 1, 5);
+let x = new Date(2019, 7, 23);
 
 
-function fn(){
+function fn() {
     let y = new Date();
 
     let a = x - y;
 
-    let DD = Math.floor(a/1000/60/60/24);
+    let DD = Math.floor(a / 1000 / 60 / 60 / 24);
 
-    let HH = Math.floor(a/1000/60/60%24);
+    let HH = Math.floor(a / 1000 / 60 / 60 % 24);
 
-    let MM = Math.floor(a/1000/60%60);
+    let MM = Math.floor(a / 1000 / 60 % 60);
 
-    let SS = Math.floor(a/1000%60);
-    
+    let SS = Math.floor(a / 1000 % 60);
+
     oWrap.innerHTML = `距离过年还有<span>${DD}</span>天<span>${HH}</span>小时<span>${MM}</span>分<span>${SS}</span>秒`;
 }
 fn();
