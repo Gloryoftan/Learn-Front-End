3e6de095e328f8d79472f04b7e89b838b6c276c6
Gloryoftan <916627606@qq.com>
2019-10-10 10:56:22 +0800

    konva-selector

--- canvas/Lecture/canvas-konva.js
+++ canvas/Lecture/canvas-konva.js
@@ -2338,3 +2338,254 @@
         );
     }
 })();
+
+/* demo-mark-19 */
+(function () {
+    let oDemo = document.querySelector('#content .demo19 .title');
+    let aLi = document.querySelectorAll('#content .demo19 ul li');
+    let konva = document.getElementById('konva19');
+    var width = 800;
+    var height = 500;
+
+    oDemo.onclick = function () {
+        konva.style.background = "hsl(0,0%,80%)";
+    }
+
+    /* Select Shape by id */
+    aLi[0].onclick = function () {
+        var stage = new Konva.Stage({
+            container: 'konva19',
+            width: width,
+            height: height
+        });
+
+        var layer = new Konva.Layer();
+        for (var n = 0; n < 10; n++) {
+            var circle = new Konva.Circle({
+                x: Math.random() * stage.width(),
+                y: Math.random() * stage.height(),
+                radius: Math.random() * 50 + 25,
+                fill: 'red',
+                strokeWidth: 3,
+                stroke: 'black'
+            });
+
+            layer.add(circle);
+        }
+
+        var rect = new Konva.Rect({
+            x: 300,
+            y: 90,
+            width: 100,
+            height: 50,
+            fill: 'green',
+            strokeWidth: 3,
+            offset: {
+                x: 50,
+                y: 25
+            },
+            draggable: true,
+            id: 'myRect'
+        });
+
+        layer.add(rect);
+        stage.add(layer);
+
+        var tween;
+
+        document.getElementById('activate1').addEventListener(
+            'click',
+            function () {
+                // or var shape = stage.findOne('#myRect');
+                var shape = stage.find('#myRect')[0];
+
+                if (tween) {
+                    tween.destroy();
+                }
+
+                tween = new Konva.Tween({
+                    node: shape,
+                    duration: 1,
+                    scaleX: Math.random() * 2,
+                    scaleY: Math.random() * 2,
+                    easing: Konva.Easings.ElasticEaseOut
+                }).play();
+            },
+            false
+        );
+    }
+
+    /* Select Shape by Type */
+    aLi[1].onclick = function () {
+        var stage = new Konva.Stage({
+            container: 'konva19',
+            width: width,
+            height: height
+        });
+
+        var layer = new Konva.Layer();
+        for (var n = 0; n < 10; n++) {
+            var circle = new Konva.Circle({
+                x: Math.random() * stage.width(),
+                y: Math.random() * stage.height(),
+                radius: Math.random() * 50 + 25,
+                fill: 'red',
+                strokeWidth: 3,
+                stroke: 'black'
+            });
+
+            layer.add(circle);
+        }
+
+        var rect1 = new Konva.Rect({
+            x: 250,
+            y: 90,
+            width: 100,
+            height: 50,
+            fill: 'green',
+            strokeWidth: 3,
+            stroke: 'black',
+            offset: {
+                x: 50,
+                y: 25
+            },
+            draggable: true
+        });
+
+        var rect2 = new Konva.Rect({
+            x: 70,
+            y: 90,
+            width: 100,
+            height: 50,
+            fill: 'green',
+            strokeWidth: 3,
+            stroke: 'black',
+            offset: {
+                x: 50,
+                y: 25
+            },
+            draggable: true
+        });
+        layer.add(rect1);
+        layer.add(rect2);
+        stage.add(layer);
+
+        var tweens = [];
+
+        document.getElementById('activate2').addEventListener(
+            'click',
+            function () {
+                // select shapes by name
+                var shapes = stage.find('Rect');
+
+                // if there are currently any active tweens, destroy them
+                // before creating new ones
+                for (var n = 0; n < tweens.length; n++) {
+                    tweens[n].destroy();
+                }
+
+                // apply transition to all nodes in the array
+                shapes.each(function (shape) {
+                    tweens.push(
+                        new Konva.Tween({
+                            node: shape,
+                            duration: 1,
+                            scaleX: Math.random() * 2,
+                            scaleY: Math.random() * 2,
+                            easing: Konva.Easings.ElasticEaseOut
+                        }).play()
+                    );
+                });
+            },
+            false
+        );
+    }
+
+    /* Select Shape by Name */
+    aLi[2].onclick = function () {
+        var stage = new Konva.Stage({
+            container: 'konva19',
+            width: width,
+            height: height
+        });
+
+        var layer = new Konva.Layer();
+        for (var n = 0; n < 10; n++) {
+            var circle = new Konva.Circle({
+                x: Math.random() * stage.width(),
+                y: Math.random() * stage.height(),
+                radius: Math.random() * 50 + 25,
+                fill: 'red',
+                strokeWidth: 3,
+                stroke: 'black'
+            });
+
+            layer.add(circle);
+        }
+
+        var rect1 = new Konva.Rect({
+            x: 250,
+            y: 90,
+            width: 100,
+            height: 50,
+            fill: 'green',
+            strokeWidth: 3,
+            stroke: 'black',
+            offset: {
+                x: 50,
+                y: 25
+            },
+            draggable: true,
+            name: 'rectangle'
+        });
+
+        var rect2 = new Konva.Rect({
+            x: 70,
+            y: 90,
+            width: 100,
+            height: 50,
+            fill: 'green',
+            strokeWidth: 3,
+            stroke: 'black',
+            offset: {
+                x: 50,
+                y: 25
+            },
+            draggable: true,
+            name: 'rectangle'
+        });
+        layer.add(rect1);
+        layer.add(rect2);
+        stage.add(layer);
+
+        var tweens = [];
+
+        document.getElementById('activate3').addEventListener(
+            'click',
+            function () {
+                // select shapes by name
+                var shapes = stage.find('.rectangle');
+
+                // if there are currently any active tweens, destroy them
+                // before creating new ones
+                for (var n = 0; n < tweens.length; n++) {
+                    tweens[n].destroy();
+                }
+
+                // apply transition to all nodes in the array
+                shapes.each(function (shape) {
+                    tweens.push(
+                        new Konva.Tween({
+                            node: shape,
+                            duration: 1,
+                            scaleX: Math.random() * 2,
+                            scaleY: Math.random() * 2,
+                            easing: Konva.Easings.ElasticEaseOut
+                        }).play()
+                    );
+                });
+            },
+            false
+        );
+    }
+})();
