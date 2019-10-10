cc1ea132bcd3c5453fdf1e6860e382cd567c707e
Gloryoftan <916627606@qq.com>
2019-10-10 10:34:57 +0800

    konva-animation

--- canvas/Lecture/canvas-konva.js
+++ canvas/Lecture/canvas-konva.js
@@ -2069,6 +2069,7 @@
         konva.style.background = "hsl(0,0%,80%)";
     }
 
+    /* Animation Template */
     aLi[0].onclick = function () {
         var stage = new Konva.Stage({
             container: 'konva18',
@@ -2089,5 +2090,251 @@
 
         layer.add(text);
         stage.add(layer);
+    }
+
+    /* Moving */
+    aLi[1].onclick = function () {
+        var stage = new Konva.Stage({
+            container: 'konva18',
+            width: width,
+            height: height
+        });
+
+        var layer = new Konva.Layer();
+
+        var hexagon = new Konva.RegularPolygon({
+            x: stage.width() / 2,
+            y: stage.height() / 2,
+            sides: 6,
+            radius: 20,
+            fill: 'red',
+            stroke: 'black',
+            strokeWidth: 4
+        });
+
+        layer.add(hexagon);
+        stage.add(layer);
+
+        var amplitude = 100;
+        var period = 2000;
+        // in ms
+        var centerX = stage.width() / 2;
+
+        var anim = new Konva.Animation(function (frame) {
+            hexagon.x(
+                amplitude * Math.sin((frame.time * 2 * Math.PI) / period) + centerX
+            );
+        }, layer);
+
+        anim.start();
+    }
+
+    /* Rotation */
+    aLi[2].onclick = function () {
+        var stage = new Konva.Stage({
+            container: 'konva18',
+            width: width,
+            height: height
+        });
+
+        var layer = new Konva.Layer();
+
+        /*
+         * leave center point positioned
+         * at the default which is the top left
+         * corner of the rectangle
+         */
+
+        var blueRect = new Konva.Rect({
+            x: 50,
+            y: 75,
+            width: 100,
+            height: 50,
+            fill: '#00D2FF',
+            stroke: 'black',
+            strokeWidth: 4
+        });
+
+        /*
+         * move center point to the center
+         * of the rectangle with offset
+         */
+        var yellowRect = new Konva.Rect({
+            x: 220,
+            y: 75,
+            width: 100,
+            height: 50,
+            fill: 'yellow',
+            stroke: 'black',
+            strokeWidth: 4,
+            offset: {
+                x: 50,
+                y: 25
+            }
+        });
+
+        /*
+         * move center point outside of the rectangle
+         * with offset
+         */
+
+        var redRect = new Konva.Rect({
+            x: 400,
+            y: 75,
+            width: 100,
+            height: 50,
+            fill: 'red',
+            stroke: 'black',
+            strokeWidth: 4,
+            offset: {
+                x: -100,
+                y: 0
+            }
+        });
+
+        layer.add(blueRect);
+        layer.add(yellowRect);
+        layer.add(redRect);
+        stage.add(layer);
+
+        // one revolution per 4 seconds
+        var angularSpeed = 90;
+        var anim = new Konva.Animation(function (frame) {
+            var angleDiff = (frame.timeDiff * angularSpeed) / 1000;
+            blueRect.rotate(angleDiff);
+            yellowRect.rotate(angleDiff);
+            redRect.rotate(angleDiff);
+        }, layer);
+
+        anim.start();
+    }
+
+    /* Scaling */
+    aLi[3].onclick = function () {
+        var stage = new Konva.Stage({
+            container: 'konva18',
+            width: width,
+            height: height
+        });
+
+        var layer = new Konva.Layer();
+
+        /*
+         * leave center point positioned
+         * at the default which is at the center
+         * of the hexagon
+         */
+        var blueHex = new Konva.RegularPolygon({
+            x: 50,
+            y: stage.height() / 2,
+            sides: 6,
+            radius: 40,
+            fill: '#00D2FF',
+            stroke: 'black',
+            strokeWidth: 4,
+            draggable: true
+        });
+
+        var yellowHex = new Konva.RegularPolygon({
+            x: 150,
+            y: stage.height() / 2,
+            sides: 6,
+            radius: 50,
+            fill: 'yellow',
+            stroke: 'black',
+            strokeWidth: 4,
+            draggable: true
+        });
+
+        /*
+         * move center point to right side
+         * of hexagon
+         */
+        var redHex = new Konva.RegularPolygon({
+            x: 300,
+            y: stage.height() / 2,
+            sides: 6,
+            radius: 50,
+            fill: 'red',
+            stroke: 'black',
+            strokeWidth: 4,
+            offset: {
+                x: 50,
+                y: 0
+            },
+            draggable: true
+        });
+
+        layer.add(blueHex);
+        layer.add(yellowHex);
+        layer.add(redHex);
+        stage.add(layer);
+
+        var period = 2000;
+
+        var anim = new Konva.Animation(function (frame) {
+            var scale = Math.sin((frame.time * 2 * Math.PI) / period) + 0.001;
+            // scale x and y
+            blueHex.scale({
+                x: scale,
+                y: scale
+            });
+            // scale only y
+            yellowHex.scaleY(scale);
+            // scale only x
+            redHex.scaleX(scale);
+        }, layer);
+
+        anim.start();
+    }
+
+    /* Stop Animation */
+    aLi[4].onclick = function () {
+        var stage = new Konva.Stage({
+            container: 'konva18',
+            width: width,
+            height: height
+        });
+
+        var layer = new Konva.Layer();
+        var hexagon = new Konva.RegularPolygon({
+            x: stage.width() / 2,
+            y: stage.height() / 2,
+            sides: 6,
+            radius: 70,
+            fill: 'red',
+            stroke: 'black',
+            strokeWidth: 4
+        });
+
+        layer.add(hexagon);
+        stage.add(layer);
+
+        var amplitude = 150;
+        // in ms
+        var period = 2000;
+        var centerX = stage.width() / 2;
+
+        var anim = new Konva.Animation(function (frame) {
+            hexagon.x(
+                amplitude * Math.sin((frame.time * 2 * Math.PI) / period) + centerX
+            );
+        }, layer);
+
+        document.getElementById('start2').addEventListener(
+            'click',
+            function () {
+                anim.start();
+            },
+            false
+        );
+
+        document.getElementById('stop2').addEventListener(
+            'click',
+            function () {
+                anim.stop();
+            },
+            false
+        );
     }
 })();
